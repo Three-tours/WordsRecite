19ae491729321feadf8c8d1c6cfc255299ab1979
Tangziyi <1174779123@qq.com>
2014-12-07 00:22:10 +0800

    fight开始阶段

--- src/com/example/wordsrecite/FightActivity.java
+++ src/com/example/wordsrecite/FightActivity.java
@@ -10,38 +10,9 @@ import android.widget.TextView;
 
 public class FightActivity extends Activity {
 
-	TextView countDownStart;
-	static Handler handler;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fight);
-		
-		handler = new Handler(callback);
-		
-		Thread t = new Thread(){
-			public void run(){
-				Message msg = new Message();
-				msg.arg1 = 1;
-				handler.sendMessage(msg);
-			}
-		};
-		t.start();
 	}
-
-	Handler.Callback callback = new Handler.Callback(){
-
-		@Override
-		public boolean handleMessage(Message msg) {
-			if(msg.arg1 == 1){
-				//
-			}
-			return true;
-		}
-		
-	};
-	
-	
-	
-
 }
