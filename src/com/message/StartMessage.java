3bc8778e4c1e1b0e06877feb27868a76fdca7c20
SSheng <2367570158@qq.com>
2014-12-06 21:18:48 +0800

    修改了StartMessage

--- src/com/message/StartMessage.java
+++ src/com/message/StartMessage.java
@@ -1,8 +1,8 @@
 package com.message;
 
 public class StartMessage extends Message{
-	public int[] numbers;
-	public StartMessage(int[] numbers){
+	public int numbers;
+	public StartMessage(int numbers){
 		this.type = Config.TYPE_START;
 		this.numbers = numbers;
 	}
